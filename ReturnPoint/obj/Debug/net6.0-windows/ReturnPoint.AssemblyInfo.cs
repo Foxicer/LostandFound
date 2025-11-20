5ca311a7282d9b1d6b190da62e812a6c207e7585
Foxicer <jaydenmeganzsx@gmail.com>
2025-11-20 10:54:25 +0800

    Latest v1.3.1

--- ReturnPoint/obj/Debug/net6.0-windows/ReturnPoint.AssemblyInfo.cs
+++ ReturnPoint/obj/Debug/net6.0-windows/ReturnPoint.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReturnPoint")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dbcf222c1716dc4564284209de52ca188a20de3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92a5fe3fd9c826d664ddc92286ad45013644622a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReturnPoint")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReturnPoint")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
